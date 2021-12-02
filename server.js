5daf57ecfd94775b91048c314c6961b2ebb70e41
Kwan Harry <harrykwan@Harry-Kwans-m1-Wife.local>
2021-12-03 02:04:21 +0800

    update

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const request = require("request");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
@@ -25,24 +26,42 @@ app.get("/", (req, res, next) => {
 });
 
 app.post("/charge", (req, res) => {
+  const courseprice = 128;
   try {
     stripe.customers
       .create({
         name: req.body.name ? req.body.name : "",
         email: req.body.email ? req.body.email : "",
         phone: req.body.phone ? req.body.phone : "",
-        address: req.body.address ? req.body.address : "",
+        // address: req.body.address ? req.body.address : "",
         source: req.body.stripeToken,
       })
       .then(
         (customer) =>
           stripe.charges
             .create({
-              amount: 100,
-              currency: "hkd",
+              amount: courseprice,
+              currency: "usd",
               customer: customer.id,
             })
-            .then(() => res.render("./completed.html"))
+            .then((x) => {
+              console.log(x);
+              res.render("./completed.html");
+              var options = {
+                method: "POST",
+                url: "https://docs.google.com/forms/u/1/d/e/1FAIpQLScDpQR2gQ4EhmkOlFX6JXWSjwDCDYMUAfZvB4qRL7xyeXy3kQ/formResponse",
+                formData: {
+                  "entry.1896806186": req.body.name ? req.body.name : "",
+                  "entry.673454844": req.body.phone ? req.body.phone : "",
+                  "entry.1920172636": req.body.email ? req.body.email : "",
+                  "entry.1648267103": courseprice,
+                },
+              };
+              request(options, function (error, response) {
+                if (error) console.log(error);
+                // console.log(response.body);
+              });
+            })
         // .then(() =>
         //   res.render(req.body.url.split("charge.html").join("completed.html"))
         // )
@@ -50,10 +69,15 @@ app.post("/charge", (req, res) => {
       // .then(() => res.render("./completed.html"))
       .catch((err) => {
         console.log(err);
-        res.send(err);
+        res.render("./charge.html", {
+          warning: err.raw.message ? err.raw.message : "Error",
+        });
       });
   } catch (err) {
-    res.send(err);
+    console.log(err);
+    res.render("./charge.html", {
+      warning: "Error",
+    });
   }
 });
 
